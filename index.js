92e74e7f4b6ab12c55ecfbc9775fd7cf198cf7ed
2md-mahmudul-hasan <2md.mahmudul.hasan@gmail.com>
2023-05-13 23:08:06 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res)=>{
+  res.send('server running')
+})
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bik86wn.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+ 
+    await client.connect();
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+   
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(port, ()=>{
+  console.log('server running')
+})
